ecc65d856b8a01eba39c1b22e8c37d6f51fc08fa
Maksim Korzh <freesoft.for.people@gmail.com>
2021-07-18 18:14:46 +0300

    renamed project toyfish, redesigning source code...

--- src/toyfish.py
+++ src/toyfish.py
@@ -1,19 +1,29 @@
 # coding: utf-8
-#####################################
-# Toyfish: YOUR FIRST CHESS PROGRAM #
-#####################################
-
 import json
 class Chess:
     def __init__(self, variant):
         with open(variant) as f:
-            self.__dict__ = json.loads(f.read())
-            self.board = list(('xxxxxxxxx' + '\n') * 2 + ' ' + ''.join([
+            settings = json.loads(f.read())
+            self.__dict__ = settings
+            start_fen = settings['start_fen']
+            row = settings['offset'] + 2
+            self.board = list((row * ' ' + '\n') * 2 + ' ' + ''.join([
                 '.' * int(c) if c.isdigit() else c
-                for c in self.fen.split()[0].replace('/', '\n ')
-            ]) + ' \n' + ('xxxxxxxxx' + '\n') * 2)
-            self.side = 0 if self.fen.split()[1] == 'w' else 1
-
+                for c in start_fen.split()[0].replace('/', ' \n ')
+            ]) + ' \n' + (row * ' ' + '\n') * 2)
+            for piece, offsets in settings['directions'].items():
+                directions = (
+                    offsets.replace('N', str(-(row + 1)))
+                           .replace('S', str(row + 1))
+                           .replace('E', '1')
+                           .replace('W', '-1')
+                ).split()
+                self.directions[piece] = [eval(d) for d in directions]
+            self.N, self.S, self.E, self.W = -(row + 1), row + 1, 1, -1
+    
+    def rotate(self):
+        self.board = list(''.join(self.board)[::-1].swapcase())
+    
     def generate_moves(self):
         move_list = []
         for square in range(len(self.board)):
@@ -26,7 +36,6 @@ class Chess:
                             while True:
                                 target_square += offset
                                 captured_piece = self.board[target_square]
-                                '''
                                 if captured_piece == ' ' or captured_piece.isupper(): break
                                 if captured_piece == 'k': return []
                                 if piece == 'P':
@@ -36,16 +45,13 @@ class Chess:
                                         if square not in self.rank_2: break
                                         if self.board[target_square + self.S] != '.': break
                                         if captured_piece != '.': break
-                                
-                                '''
                                 move_list.append({
                                     "source": square,
                                     "target": target_square,
                                     "piece": piece,
                                     "captured": captured_piece
                                 })
-                                
-                                #if captured_piece.islower(): break
+                                if captured_piece.islower(): break
                                 if piece in self.leapers: break
         return move_list
                                 
@@ -102,7 +108,6 @@ class Chess:
             input()
 
 if __name__ == '__main__':
-    chess = Chess('settings.json')
-    #print(''.join(chess.board))
-    print(json.dumps(chess.directions))
+    chess = Chess('chess.json')
+    chess.game_loop()
     
