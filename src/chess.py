78c88500f2af94da5062eac2323b99e9f7ea168d
Maksim Korzh <freesoft.for.people@gmail.com>
2021-07-18 03:15:43 +0300

    implemented pawn moves

--- src/chess.py
+++ src/chess.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+# %load chess.py
 import json
 class Chess:
     def __init__(self, variant):
@@ -6,6 +7,12 @@ class Chess:
             settings = json.loads(f.read())
             fen = settings['fen']
             row = settings['offset'] + 2
+            self.variant = settings['variant']
+            self.leapers = settings['leapers']
+            self.N, self.S = -(row + 1), row + 1
+            self.E, self.W = 1, -1
+            self.rank_2 = settings['rank_2']
+            self.rank_7 = settings['rank_7']
             self.board = list((row * 'x' + '\n') * 2 + 'x' + ''.join([
                 '.' * int(c) if c.isdigit() else c
                 for c in fen.split()[0].replace('/', 'x\nx')
@@ -24,26 +31,36 @@ class Chess:
         return list(''.join(self.board)[::-1].swapcase())
     
     def generate_moves(self):
-        board, directions = self.board, self.directions
-        for square in range(len(board)):
-            piece = board[square]
+        for square in range(len(self.board)):
+            piece = self.board[square]
             if piece not in '.x\n' and piece.isupper():
-                for piece_type, offsets in directions.items():
+                for piece_type, offsets in self.directions.items():
                     if piece == piece_type:
                         for offset in offsets:
                             target_square = square
                             while True:
                                 target_square += offset
-                                captured_piece = board[target_square]
-                                if captured_piece == 'x': break
-                                
-                                board[target_square] = piece
-                                board[square] = '.'
+                                captured = self.board[target_square]
+                                if captured == 'x' or captured.isupper(): break
+                                if self.variant == 'chess':
+                                    if piece == 'P':
+                                        if offset == self.directions['P'][0] and captured != '.': break
+                                        if offset in self.directions['P'][1: -1] and captured == '.': break
+                                        if offset == self.directions['P'][-1]:
+                                            if square not in self.rank_2: break
+                                            if self.board[target_square + self.S] != '.': break
+                                            if captured != '.': break
+                                        
+                                      
+                                self.board[target_square] = piece
+                                self.board[square] = '.'
                                 print(''.join(self.board)); input()
                                 
-                                board[target_square] = captured_piece
-                                board[square] = piece
+                                self.board[target_square] = captured
+                                self.board[square] = piece
                                 print(''.join(self.board)); input()
+                                
+                                if piece in self.leapers: break
         
 if __name__ == '__main__':
     chess = Chess('chess.json')
