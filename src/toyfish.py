60b907244b60f382ad03db7a18191e4100c006a8
Maksim Korzh <freesoft.for.people@gmail.com>
2021-07-19 17:23:34 +0300

    changed filename variable

--- src/toyfish.py
+++ src/toyfish.py
@@ -1,7 +1,7 @@
 import json
 class Chess:
-    def __init__(self, variant):
-        with open(variant) as f:
+    def __init__(self, filename):
+        with open(filename) as f:
             self.__dict__ = json.loads(f.read())
             self.board = list('         \n' * 2 + ' ' + ''.join([
                 '.' * int(c) if c.isdigit() else c
